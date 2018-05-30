7efe88072d41bc4d66877163a61c58edad48cffd
Jon Yardley <jon.yardley@red-badger.com>
2018-05-30 12:47:24 +0100

    Create item list powered from App state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Header from "./Header";
 
 class App extends Component {
   state = {
-    name: "Jon Yardley"
+    name: "Jon Yardley",
+    items: [{ label: "My first item" }]
   };
   render() {
-    return <Header name={this.state.name} />;
+    return (
+      <div>
+        <Header name={this.state.name} />
+        <ul>{this.state.items.map(item => <li>{item.label}</li>)}</ul>
+      </div>
+    );
   }
 }
 
